a2123482cf104b5e6639fffa0c97d28213598ac0
T N <theexp111@gmail.com>
2018-05-07 13:36:11 +0200

    fuckmeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

--- HomoMa II/main.cpp
+++ HomoMa II/main.cpp
@@ -20,14 +20,16 @@ double g(CMyVektor a)
 }
 
 const double h = 0.00000001;
-void gradientenverfahren(CMyVektor a, double(*function)(CMyVektor x));
+void gradientenverfahren(CMyVektor a, double(*function)(CMyVektor x), double lambda);
 
 int main()
 {
-	CMyVektor myVec = CMyVektor(2);
+	/*CMyVektor myVec = CMyVektor(2);
 	myVec.Set(0, 3);
 	myVec.Set(1, 2);
-	gradientenverfahren(myVec, &f);
+	gradientenverfahren(myVec, &f);*/
+	CMyVektor myVec = CMyVektor(3);
+	gradientenverfahren(myVec, &g, 0.1);
 	system("PAUSE");
 }
 
@@ -43,33 +45,46 @@ CMyVektor gradient(CMyVektor x, double(*funktion)(CMyVektor x))
 	return x;
 }
 
-void gradientenverfahren(CMyVektor a, double(*function)(CMyVektor x))
+void gradientenverfahren(CMyVektor a, double(*function)(CMyVektor x), double halfLife3 = 1.0)
 {
 	CMyVektor x = a;
-	double lambda = 1.0f;
-	CMyVektor xNeu = x + gradient(x, function) * lambda;
+	double lambda = halfLife3;
 
 	unsigned schritt = 0;
+	CMyVektor gradfX = CMyVektor(0);
 	do
 	{
-		cout << "Schritt " << schritt << ":" << endl;
+		cout << endl << "Schritt " << schritt << ":" << endl;
 		cout << "x = " << x.ToString() << endl;
 		cout << "lambda = " << lambda << endl;
+		CMyVektor xNeu = x + gradient(x, function) * lambda;
 		double fXNeu = function(xNeu);
 		double fX = function(x);
 		cout << "f(x) = " << fX << endl;
-		cout << "grad f(x) = " << gradient(x, &f).ToString() << endl;
+		gradfX = gradient(x, function);
+		cout << "grad f(x) = " << gradfX.ToString() << endl;
+		cout << "||grad f(x)|| = " << gradfX.GetLength() << endl << endl;
+
+		cout << "x_neu = " << xNeu.ToString() << endl;
+		cout << "f(x_neu) = " << fXNeu << endl << endl;
+
 		if (fXNeu > fX)
 		{
+			cout << "Test mit doppelter Schrittweite (lambda = 2) :" << endl;
 			CMyVektor xTest = x + gradient(x, function) * (2 *lambda);
-			if (function(xTest) > fXNeu)
+			cout << "x_test = " << xTest.ToString() << endl;
+			double fXTest = function(xTest);
+			cout << "f(x_test) = " << fXTest << endl;
+			if (fXTest > fXNeu)
 			{
 				x = xTest;
 				lambda *= 2;
+				cout << "verdoppele Schrittweite!" << endl;
 			}
 			else
 			{
 				x = xNeu;
+				cout << "behalte alte Schrittweite!" << endl;
 			}
 		}
 		else
@@ -77,10 +92,32 @@ void gradientenverfahren(CMyVektor a, double(*function)(CMyVektor x))
 			do
 			{
 				lambda *= 0.5;
+				cout << "halbiere Schrittweite (lambda = " << lambda << "):" << endl;
 				xNeu = x + gradient(x, function) * lambda;
-			} while (fX > function(xNeu));
+				cout << "x_neu = " << xNeu.ToString() << endl;
+				fXNeu = function(xNeu);
+				cout << "f(x_neu) = " << fXNeu << endl;
+			} while (fX > fXNeu);
+
+			x = xNeu;
 		}
 
 		schritt++;
-	} while (schritt < 50);
+	} while (gradient(x, function).GetLength() >= 0.00001 && schritt < 50);
+
+
+	if (schritt >= 50)
+	{
+		cout << endl << "Ende wegen Schrittanzahl = 50 bei" << endl;
+		
+	}
+	else
+	{
+		cout << endl << "Ende wegen ||grad f(x)||<1e-5 bei" << endl;
+	}
+	cout << "x = " << x.ToString() << endl;
+	cout << "lambda = " << lambda << endl;
+	cout << "f(x) = " << function(x) << endl;
+	cout << "grad f(x) = " << gradient(x, function).ToString() << endl;
+	cout << "||grad f(x)|| = " << gradient(x, function).GetLength() << endl;
 }
