3beb7e5500aa27424d766e507596aded54f5ef98
Exp <theexp111@gmail.com>
2018-06-21 17:34:29 +0200

    shitty stuff i don't this is correct (this is just normal probability stuff; nothing much monte carlo-y about it i guess)

--- P5/main.cpp
+++ P5/main.cpp
@@ -4,6 +4,27 @@
 #include "CLotto.h"
 using namespace std;
 
+int monteCarloStuff(CLotto lotto, int numZiehungen, int (*func)(CLotto), int wantedEreignis)
+{
+	int timesItHappened = 0;
+	for (int i = 0; i < numZiehungen; i++)
+	{
+		if (func(lotto) == wantedEreignis)
+			timesItHappened++;
+	}
+	return timesItHappened;
+}
+
+int rapper(CLotto lotto)
+{
+	return lotto.lottoZiehung();
+}
+
+int lilpump(CLotto lotto)
+{
+	return lotto.lotto2Ziehung();
+}
+
 int main()
 {
 	/*CZufall pseudoRand;
@@ -21,9 +42,15 @@ int main()
 		cout << testFalsch[i] << endl;
 	}*/
 
-	CLotto lotto = CLotto(100);
+	/*CLotto lotto = CLotto(100);
 	lotto.setTipp(1, 2, 3, 4, 5, 6);
 	cout << lotto.lottoZiehung(true) << endl;
-	cout << lotto.lotto2Ziehung(true) << endl;
+	cout << lotto.lotto2Ziehung(true) << endl;*/
+
+	const int monteCarloZiehungen = 1000000;
+	CLotto lotto = CLotto(1337);
+	lotto.setTipp(2, 6, 9, 13, 28, 42);
+	cout << (double)monteCarloStuff(lotto, monteCarloZiehungen, &rapper, 3) / monteCarloZiehungen << endl;
+	cout << (double)monteCarloStuff(lotto, monteCarloZiehungen, &lilpump, 3) / monteCarloZiehungen << endl;
 	system("PAUSE");
 }
